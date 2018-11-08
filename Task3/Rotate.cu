d970b8dcbf5595b8098c0a04a9cb01cdc4021364
JayChen <394715636@qq.com>
2018-11-08 23:28:58 +0800

    这些好了，所有功能都优化成功了！要不尝试一下opencl版的？

--- Task3/Rotate.cu
+++ Task3/Rotate.cu
@@ -16,7 +16,8 @@ inline void checkCudaErrors(cudaError err) //cuda error handle function
 	}
 }
 
-__global__ void Rotate_kernel(byte *GPU_source, byte *GPU_result, int HandleWidth, int HandleHeight, int SourceWidth, int SourceHeight, double angle)
+__global__ void Rotate_kernel(byte *GPU_source, byte *GPU_result, int HandleWidth, int HandleHeight, int SourceWidth, int SourceHeight, double angle,
+	int source_pitch, int source_pixelSize, int handle_pitch, int handle_pixelSize)
 {
 	//x，y坐标即为对应的线程在整个thread阵里面的x，y坐标
 	int x = blockIdx.x*blockDim.x + threadIdx.x;
@@ -42,7 +43,9 @@ __global__ void Rotate_kernel(byte *GPU_source, byte *GPU_result, int HandleWidt
 	if (origin.x < 1 || origin.y < 1 || origin.x >= SourceWidth - 2 || origin.y >= SourceHeight - 2)
 	{
 		//图像边缘使用最近邻插值法
-		GPU_result[y*HandleWidth + x] = GPU_source[(int)origin.y*SourceWidth + (int)origin.x];
+		GPU_result[(HandleHeight - 1 - y)*(-1)*handle_pitch + x * handle_pixelSize] = GPU_source[(SourceHeight - 1 - (int)origin.y)*(-1)*source_pitch + (int)origin.x * source_pixelSize];
+		GPU_result[(HandleHeight - 1 - y)*(-1)*handle_pitch + x * handle_pixelSize + 1] = GPU_source[(SourceHeight - 1 - (int)origin.y)*(-1)*source_pitch + (int)origin.x * source_pixelSize + 1];
+		GPU_result[(HandleHeight - 1 - y)*(-1)*handle_pitch + x * handle_pixelSize + 2] = GPU_source[(SourceHeight - 1 - (int)origin.y)*(-1)*source_pitch + (int)origin.x * source_pixelSize + 2];
 	}
 	else
 	{
@@ -54,18 +57,28 @@ __global__ void Rotate_kernel(byte *GPU_source, byte *GPU_result, int HandleWidt
 		double weights[4][4];
 		getWeight(weights, origin.x, origin.y);
 
-		double value = 0;
+		double value_R = 0;
+		double value_G = 0;
+		double value_B = 0;
 		for (int y = 0; y < 4; y++)
 		{
 			for (int x = 0; x < 4; x++)
 			{
-				value += GPU_source[neighbors[x][y].y*SourceWidth + neighbors[x][y].x] * weights[x][y];
+				value_R += GPU_source[(SourceHeight - 1 - neighbors[x][y].y)*(-1)*source_pitch + neighbors[x][y].x * source_pixelSize] * weights[x][y];
+				value_G += GPU_source[(SourceHeight - 1 - neighbors[x][y].y)*(-1)*source_pitch + neighbors[x][y].x * source_pixelSize + 1] * weights[x][y];
+				value_B += GPU_source[(SourceHeight - 1 - neighbors[x][y].y)*(-1)*source_pitch + neighbors[x][y].x * source_pixelSize + 2] * weights[x][y];
 			}
 		}
 
-		value = value < 0 ? 0 : value;
-		value = value > 255 ? 255 : value;
-		GPU_result[y*HandleWidth + x] = (byte)value;
+		value_R = value_R < 0 ? 0 : value_R;
+		value_R = value_R > 255 ? 255 : value_R;
+		value_G = value_G < 0 ? 0 : value_G;
+		value_G = value_G > 255 ? 255 : value_G;
+		value_B = value_B < 0 ? 0 : value_B;
+		value_B = value_B > 255 ? 255 : value_B;
+		GPU_result[(HandleHeight - 1 - y)*(-1)*handle_pitch + x * handle_pixelSize] = (byte)value_R;
+		GPU_result[(HandleHeight - 1 - y)*(-1)*handle_pitch + x * handle_pixelSize + 1] = (byte)value_G;
+		GPU_result[(HandleHeight - 1 - y)*(-1)*handle_pitch + x * handle_pixelSize + 2] = (byte)value_B;
 	}
 }
 
@@ -81,14 +94,15 @@ extern "C" void Rotate_host(byte* source, byte* result_buf, int HandleWidth, int
 	byte* GPU_result;
 
 	//在显存中为原图像和工作区分配空间
-	checkCudaErrors(cudaMalloc((void **)&GPU_source, sizeof(byte)*SourceWidth*SourceHeight));
-	checkCudaErrors(cudaMalloc((void **)&GPU_result, sizeof(byte)*HandleWidth*HandleHeight));
+	checkCudaErrors(cudaMalloc((void **)&GPU_source, sizeof(byte)*SourceHeight*((-1)*source_pitch)));
+	checkCudaErrors(cudaMalloc((void **)&GPU_result, sizeof(byte)*HandleHeight*((-1)*handle_pitch)));
 
-	checkCudaErrors(cudaMemcpy(GPU_source, source, sizeof(byte)*SourceHeight*SourceWidth, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(GPU_source, source, sizeof(byte)*SourceHeight*((-1)*source_pitch), cudaMemcpyHostToDevice));
 
-	Rotate_kernel << < DimGrid, DimBlock >> > (GPU_source, GPU_result, HandleWidth, HandleHeight, SourceWidth, SourceHeight, angle);
+	Rotate_kernel << < DimGrid, DimBlock >> > (GPU_source, GPU_result, HandleWidth, HandleHeight, SourceWidth, SourceHeight, angle,
+		source_pitch, source_pixelSize, handle_pitch, handle_pixelSize);
 
-	checkCudaErrors(cudaMemcpy(result_buf, GPU_result, sizeof(byte)*HandleWidth*HandleHeight, cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaMemcpy(result_buf, GPU_result, sizeof(byte)*HandleHeight*((-1)*handle_pitch), cudaMemcpyDeviceToHost));
 
 	cudaFree(GPU_source);
 	cudaFree(GPU_result);
